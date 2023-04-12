b2898685b9838b9cb8084bf22366b945eb641c7a
Andrew <andrew@unai.edu>
2023-04-12 11:24:05 +0700

    working LoginController

--- routes/api.php
+++ routes/api.php
@@ -1,30 +1,30 @@
 <?php
 
+// Import necessary classes
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProductController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+// Define login route
+Route::post('/login', [LoginController::class, 'login']);
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+// Protect the routes that require authentication using Sanctum middleware
+Route::middleware('auth:sanctum')->group(function () {
 
-Route::apiResource('products', ProductController::class);
+    // Define routes for products resource controller
+    Route::apiResource('products', ProductController::class);
 
-Route::get('/users', [UserController::class, 'index']);
-Route::post('/users', [UserController::class, 'store']);
-Route::get('/users/{user}', [UserController::class, 'show']);
-Route::put('/users/{user}', [UserController::class, 'update']);
-Route::delete('/users/{user}', [UserController::class, 'destroy']);
+    // Define routes for user resource controller
+    Route::get('/users', [UserController::class, 'index']);
+    Route::post('/users', [UserController::class, 'store']);
+    Route::get('/users/{user}', [UserController::class, 'show']);
+    Route::put('/users/{user}', [UserController::class, 'update']);
+    Route::delete('/users/{user}', [UserController::class, 'destroy']);
 
+    // Define logout route
+    Route::post('/logout', [LoginController::class, 'logout']);
+});
+
+?>
